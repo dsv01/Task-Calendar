cdebb100f03669f1c5d3fe2a7a46901dd32b84e2
DSV01 <dsv@dcc.ufrj.br>
2024-04-27 15:21:08 -0300

    feature: Criando layout da página home

---
+++ front/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { HomeDiv, Header, Content, Footer } from "./style";
+
+export default function Home()
+{
+    return (
+        <HomeDiv>
+            <Header></Header>
+            <Content></Content>
+            <Footer></Footer>
+        </HomeDiv>
+    )
+}
---
+++ front/src/pages/Home/style.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const HomeDiv = styled.div `
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+`;
+
+export const Header = styled.div `
+    display: flex;
+    flex-direction: row;
+
+    height: 15%;
+
+    background-color: #aaaccc;
+`;
+
+export const Content = styled.div `
+    display: flex;
+    flex-direction: row;
+
+    height: 75%;
+
+    // background-color: blue;
+`;
+
+export const Footer = styled.div `
+    display: flex;
+    flex-direction: row;
+
+    height: 10%;
+
+    background-color: #aaaccc;
+`;
