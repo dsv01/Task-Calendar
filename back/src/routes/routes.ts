97f24686db38f6e44b89a27767ff386211a2add1
DSV01 <dsv@dcc.ufrj.br>
2024-04-30 21:20:16 -0300

    feature: Adicionando endpoints de task

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import taskController from "../controllers/taskController";
 
 const router = Router();
+
+router.post("/task/createTask", taskController.createTask);
+router.get("/task/showTask", taskController.showTask);
+router.put("/task/updateTask", taskController.updateTask);
+router.delete("/task/destroyTask", taskController.destroyTask);
 
 export default router;
